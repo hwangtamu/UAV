fe40685af0babea4ce5d29521c40aa5a0164d09a
Han Wang <hanwang@tamu.edu>
2013-11-22 11:19:41 -0600

    Update PersonMove2.js

--- PersonMove2.js
+++ PersonMove2.js
@@ -6,6 +6,8 @@ var startPos = Vector3.zero;
 var trans = 0.0;
 var xMove = 0.0;
 var zMove = 0.0;
+var x_target = 0.0;
+var z_target = -5.5;
 
 //target variables
 var target;
@@ -18,16 +20,17 @@ var personal = 1.22;
 var social = 3.66;
 var sqrLen = 0.0;
 
+
+
 function Start(){
-	//random returns a float f such that 0 <= f <= 1
+	//random returns a float f such thatr 0 <= f <= 1
 	var rand = Random.value;
 	//movement in x direction
 	//transform into a value between -.5 and .5 (+/- 45 degrees)
 	trans = rand - 0.5;
-	
 	//determine random speed (at least 0.3 so it's not completely tedious)
 	rand = Random.value;
-	speed = rand + 0.3;
+	speed = rand/2+0.3;
 	
 	//determine random starting position within bounding box
 	// -2 < x < 2
@@ -38,14 +41,21 @@ function Start(){
 	var startZ = -1*rand - 10;
 	startPos = new Vector3(startX, 0.33, startZ);
 	transform.position = startPos;
+	x_target = trans*3.0;
+	var travel_route = 	Mathf.Sqrt((x_target-startX)*(x_target-startX)+(z_target-startZ)*(z_target-startZ));
+	var t = travel_route/speed;
+	xMove = (x_target-startX)*0.03/t;
+	zMove = (z_target-startZ)*0.03/t;
 	
 }
 
 function Update () {
+
 	//determines tilt-ness
-	xMove = trans * Time.deltaTime * speed;
+	//xMove = trans * Time.deltaTime * speed;
+	
 	//move forward in z direction
-	zMove = 1.0 * Time.deltaTime * speed;
+	//zMove = 1.0 * Time.deltaTime * speed;
 	
 	//determine distance between me and robot
 	sqrLen = (target.position - transform.position).sqrMagnitude;
@@ -73,4 +83,8 @@ function Update () {
 	if(transform.position.z < 0.9){
 		transform.Translate(xMove, 0, zMove);
 	}
+	if(transform.position.z<-14 || transform.position.z>0||(Mathf.Abs(transform.position.x)>2.5 && Mathf.Abs(transform.position.z-5.5)<0.1)){
+		Destroy(gameObject);
+	}
+	//Destroy(gameObject,10.0);
 }
